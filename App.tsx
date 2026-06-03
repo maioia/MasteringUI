38afbd62d5ac76de8573cf8d611e7f638ff552f2
Mai.A.K <mai_e_engineer@yahoo.com>
2026-06-03 14:49:54 +0200

    start add Home screen

--- App.tsx
+++ App.tsx
@@ -1,24 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import IntroScreen from "./src/screens/IntroScreen";
-import HomeScreenSun from "./src/screens/components/HomeScreenSun/HomeScreenSun";
-import ContactScreen from "./src/screens/components/ContactScreen/ContactScreen";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ContactScreen />
+      <HomeScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    
-  },
-  sunImg: {
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-  },
+  container: {},
 });
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { s, vs } from "react-native-size-matters";
+
+const HomeScreen = () => {
+  return (
+    <View style={{ marginTop: vs(50), paddingHorizontal: s(16) }}>
+      <Text
+        style={{
+          fontSize: s(20),
+          fontWeight: "semibold",
+          color: "#1D150F",
+          marginBottom: vs(6),
+        }}
+      >
+        Meditations
+      </Text>
+      <Text style={{ color: "#2C2016",fontSize:s(14) }}>Lorem Ipsum is simply dummy text</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
